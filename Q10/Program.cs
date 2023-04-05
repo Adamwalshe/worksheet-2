19bc376efb6e6be3257b02fe8c5d69e4595445b8
Adamwalshe <123950051+Adamwalshe@users.noreply.github.com>
2023-04-05 19:48:52 +0100

    Add files via upload

---
+++ Q10/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace Q10
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DistanceTravelled();
+        }
+        static private void DistanceTravelled()
+        {
+            Console.Write("Speed of vehicle in kilometres per hour:");
+            int speed = int.Parse(Console.ReadLine());
+            Console.Write("Time spent traveling in hours:");
+            int time = int.Parse(Console.ReadLine());
+            Console.WriteLine($"{"Hour",-10} Distance Travelled");
+            for (int i = 1; i<=time; i++) 
+            {                
+                int hourlyspeed = speed * i;
+                int distance = hourlyspeed * i;
+
+                Console.Write($"{i,-3} {hourlyspeed,-5} {distance}");
+                Console.WriteLine("");
+            }
+
+
+
+        }
+    }
+}
---
+++ Q11/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace Q11
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("How many years of rainfall:");
+            int years = int.Parse(Console.ReadLine());
+            for (int i = 1; i <= years; i++) 
+            {
+                Loop();
+                
+            }
+        }
+        static private void Loop()
+        {
+            for (int j = 1; j <= 12; j++)
+            {
+                int[] rain = new int[12];
+                Console.Write($"Enter rainfall in cm for month {j}:");
+                string xstring = Console.ReadLine();
+                rain[j] = int.Parse(xstring);
+            }
+
+        }
+    }
+}
