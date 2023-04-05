f38b42da6cbb83db6c062bd632ecc87c0acc18ac
Adamwalshe <123950051+Adamwalshe@users.noreply.github.com>
2023-04-05 19:47:53 +0100

    Add files via upload

---
+++ Q7/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace Q7
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DiscountRates();
+        }
+        static private void DiscountRates()
+        {
+            Console.Write("How many children have you for the holiday: ");
+            int numOfKids = int.Parse(Console.ReadLine());
+            if (numOfKids == 0) 
+            {
+                Console.WriteLine("Your discount is 0%");
+            }
+            else if (numOfKids >= 1 && numOfKids <=2 )
+            {
+                Console.WriteLine("Your discount is 5%");
+            }
+            else if ( numOfKids >= 3 && numOfKids <=5 ) 
+            {
+                Console.WriteLine("Your discount is 10%");
+            }
+            else if (numOfKids > 5 )
+            {
+                Console.WriteLine("Your discount is 15%");
+            }
+            else
+            {
+                Console.WriteLine("");
+            }
+
+
+        }
+    }
+}
---
+++ Q8/Program.cs
@@ -0,0 +1,48 @@
+﻿namespace Q8
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Rental();
+        }
+        static private void Rental()
+        {
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+            int charge1 = 10;
+            int charge2 = 8;
+            int initialCharge3 = 15;
+            int totalCharges;
+            int x;
+            int y;
+            int z;
+
+            Console.Write("Enter number of days for rental:");
+            int numOfDays = int.Parse(Console.ReadLine());
+
+            if (numOfDays == 0)
+            {
+                Console.WriteLine("Charge is 0");
+            }
+            else if (numOfDays >=1 && numOfDays <=5)
+            {
+                z = numOfDays * charge1;
+                totalCharges = z + initialCharge3;
+                Console.WriteLine($"Your total charge is: {totalCharges:c}");
+            }
+            else if ( numOfDays >= 6 )
+            {
+                x = charge1 * 5;
+                y = charge2 * (numOfDays-5);
+                totalCharges = x + y + initialCharge3; 
+                Console.WriteLine($"Your total charge is: {totalCharges:c}");
+            }
+            else
+            {
+                Console.WriteLine("Invalid amount of days inputed");
+            }
+
+
+        }
+    }
+}
