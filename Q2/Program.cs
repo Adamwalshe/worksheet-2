2144431da288f16ecefc81d416592d162c167c34
Adamwalshe <123950051+Adamwalshe@users.noreply.github.com>
2023-04-05 19:43:35 +0100

    Add files via upload

---
+++ Q2/Program.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.VisualBasic;
+
+namespace Q2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            IsSorted();
+        }
+        static bool IsSorted()
+        {
+            int num;
+            Console.WriteLine("Enter length of array:");
+            num = int.Parse(Console.ReadLine());
+            int[]x = new int[num];
+            for (int i = 0; i < num; i = i + 1)
+            {
+                string xstring = Console.ReadLine();
+                x[i] = int.Parse(xstring);
+            }
+            for (int j = 0; j < x.Length - 1; j++)
+            {
+                if (x[j] > x[j + 1])
+                {
+                    Console.WriteLine("My array is not sorted");
+                    return false;
+                }
+            }
+            Console.WriteLine("My array is sorted");
+            return true;
+
+
+
+
+
+
+        }
+
+    }
+}
---
+++ Q3/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace Q3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            AgeBracket();
+        }
+        static bool AgeBracket()
+        {
+            int age;
+            Console.WriteLine("Enter age: ");
+            age = int.Parse(Console.ReadLine());
+            if (age <18 && age >21)
+            {
+                return false;
+            }
+            else if (age >18 && age <21) 
+            
+                Console.WriteLine("You can access this website");
+                return true; 
+            
+            
+
+
+        }
+    }
+}
