1a135d07e80bc0f4aa869c58dcb59c992bf7c319
adamw <adamw@ADAMSLAPTOP>
2023-04-05 19:39:06 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿namespace worksheet_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Sum();
+        }
+        static private void Sum()
+        {
+            int n1;
+            int n2;
+            int sum = 0;
+            Console.Write("Enter lower number: ");
+            n1 = int.Parse(Console.ReadLine());
+            Console.Write("Enter higher number: ");
+            n2 = int.Parse(Console.ReadLine());
+            
+            for (int i = n1; i <= n2; i++  )
+            {
+                if (i % 2 == 1)
+                {
+                    sum = sum + i;
+                }
+                
+            }
+            Console.WriteLine(sum); 
+            
+        }
+
+    }
+}
